72675f1023240831d3226db06990606965bb679d
Eli Fatsi <eli.fatsi@viget.com>
2019-08-05 15:07:03 -0600

    Fix game creation (remove opentok API calls)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,11 +10,11 @@ class GamesController < ApplicationController
   end
 
   def show
-    api_key = "17052081"
-    api_secret = "0d219ab9c3e5a7bf92fec80ba458f0e7ba6a3a2d"
-    @opentok = OpenTok::OpenTokSDK.new api_key, api_secret if @opentok.nil?
-    @token = @opentok.generate_token(:session_id => @game.session_id)
-    
+    # api_key = "17052081"
+    # api_secret = "0d219ab9c3e5a7bf92fec80ba458f0e7ba6a3a2d"
+    # @opentok = OpenTok::OpenTokSDK.new api_key, api_secret if @opentok.nil?
+    # @token = @opentok.generate_token(:session_id => @game.session_id)
+
     @game.add_player_from_user(current_user)
     respond_to do |format|
       format.html
@@ -41,13 +41,13 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(params[:game])
 
-    api_key = "17052081"
-    api_secret = "0d219ab9c3e5a7bf92fec80ba458f0e7ba6a3a2d"
-    @opentok = OpenTok::OpenTokSDK.new api_key, api_secret
-    
-    session = @opentok.create_session request.remote_addr
-    @game.update_attributes(:session_id => session.session_id)
-    
+    # api_key = "17052081"
+    # api_secret = "0d219ab9c3e5a7bf92fec80ba458f0e7ba6a3a2d"
+    # @opentok = OpenTok::OpenTokSDK.new api_key, api_secret
+    #
+    # session = @opentok.create_session request.remote_addr
+    # @game.update_attributes(:session_id => session.session_id)
+
 
     if @game.save
       redirect_to @game
@@ -60,7 +60,7 @@ class GamesController < ApplicationController
     @game.destroy
     redirect_to games_url
   end
-  
+
   def reload
     reload_partial
   end
